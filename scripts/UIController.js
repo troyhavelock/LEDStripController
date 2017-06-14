ba1f822a7a04a88c2cff90f82aec4ae0d619bcb8
Troy Havelock <troyhavelock@cmail.carleton.ca>
2017-06-14 11:39:08 -0400

    Added two directories: scripts and styles, for better organization

---
+++ scripts/UIController.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("UISidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("UISidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
